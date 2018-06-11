b6ff3969cfa741afd863a8194d65ed7f8d2bb881
linhdvu14 <linhvu.14@gmail.com>
2018-06-11 19:05:04 +0700

    reorganize files

--- utils/mix_noise.sh
+++ utils/mix_noise.sh
@@ -1,24 +1,28 @@
+NOISE="18_Fan_USB"
+
 NOISE_DIR="data/wav/noises/"
-NOISE="bus-interior-1"
 NOISE_FILE="${NOISE_DIR}${NOISE}.wav"
+SPEECH_DIR="data/wav/prdcv_template/"
 
-SPEECH_DIR="data/wav/prdcv/"
 
-OUT_DIR=${SPEECH_DIR/prdcv/prdcv_${NOISE}}
+# Mix template (clean) audios with specified noise
+OUT_DIR="data/wav/prdcv_mixed/prdcv_${NOISE}/"
+rm -rf $OUT_DIR
 mkdir $OUT_DIR
 
 
 for f in $(find $SPEECH_DIR -name '*.wav'); do 
-	OUT_FILE=${f/prdcv/prdcv_${NOISE}}
+	fn=`echo $f | rev | cut -d \/ -f 1 | rev`  # filename w/o path
+	OUT_FILE="${OUT_DIR}/$fn"
 
-	# convert to 48kHz mono
-	sox $f -r 48k -c 1 temp_speech.wav
-	sox $NOISE_FILE -r 48k -c 1 temp_noise.wav
+	# convert to 16kHz mono
+	sox $f -r 16k -c 1 temp_speech.wav
+	sox $NOISE_FILE -r 16k -c 1 temp_noise.wav
 
-	# concat noise sample to >= speech sample
+	# concat noise sample to >= length of speech sample
 	len_noise=`soxi -D temp_noise.wav | cut -d \. -f 1`  # drop decimal
 	len_speech=`soxi -D temp_speech.wav | cut -d \. -f 1`
-	t=$((len_speech / len_noise ))
+	t=$(( len_speech / len_noise ))
 
 	cp temp_noise.wav temp_noise2.wav
 	for ((i=0; i < t; i++)); do
@@ -27,13 +31,33 @@ for f in $(find $SPEECH_DIR -name '*.wav'); do
 	done
 
 	# merge and trim
-	sox -m temp_speech.wav temp_noise2.wav temp_mixed.wav trim 0 `soxi -D temp_speech.wav`
-	# sox -m -v 1 temp_speech.wav -v 0.3 temp_noise2.wav temp_mixed.wav trim 0 `soxi -D temp_speech.wav`
+	# sox -m temp_speech.wav temp_noise2.wav temp_mixed.wav trim 0 `soxi -D temp_speech.wav`
+	sox -m -v 1 temp_speech.wav -v 3 temp_noise2.wav temp_mixed.wav trim 0 `soxi -D temp_speech.wav`
 
-	# down to 16kHz
+	# output
 	sox temp_mixed.wav -r 16k -c 1 $OUT_FILE
 
 	# clean up
 	rm -f temp*
 
+done
+
+
+
+# Split mixed audio into short segments and rename
+SEGMENT_LENGTH=4  # secs
+
+for f in $(find $OUT_DIR -name '*.wav'); do 
+	fn=`echo $f | cut -d \. -f 1`  # drop extension
+	sox $f $fn-.wav trim 0 $SEGMENT_LENGTH : newfile : restart  # split audio
+done
+
+
+for f in $(find $OUT_DIR -name '*.wav'); do 
+	length=`soxi -D $f`
+	if [ $(echo " $SEGMENT_LENGTH == $length" | bc) -eq 1 ]; then  # rename split segments
+		mv $f ${f/120s_/${SEGMENT_LENGTH}s_}
+	elif (( $(echo "$SEGMENT_LENGTH > $length" |bc -l) )); then
+		rm $f
+	fi
 done
